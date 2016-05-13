f8676d70f92d109ce180b024c399702ea7182d37
tjniv1 <tjniv1@082-123-003WL.AD.MONASH.EDU>
2016-05-13 17:44:43 +1000

    Added in basic user interface for addLocation page

--- Assignment 2/code/js/addlocationPage.js
+++ Assignment 2/code/js/addlocationPage.js
@@ -1,3 +1,9 @@
 /*jslint white: true */
 // Code for the Add Location page.
 
+// Getting location name from field
+function submitLocation()
+{
+    var locationName = document.getElementById("locationName").value;
+    alert(locationName);
+}
